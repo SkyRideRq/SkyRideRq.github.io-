0a934cb50916618809aa21a97f11b22747a20011
SkyRideRq <piotrslowinski@op.pl>
2019-01-08 13:08:17 +0100

    Updates

---
+++ precache-manifest.2edfb5cb1ba4d386c29869be1f75a4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "19bee67087b81e75cea5",
+    "url": "/static/js/main.19bee670.chunk.js"
+  },
+  {
+    "revision": "033cdf4a086cad5b4a40",
+    "url": "/static/js/1.033cdf4a.chunk.js"
+  },
+  {
+    "revision": "19bee67087b81e75cea5",
+    "url": "/static/css/main.1a2cf200.chunk.css"
+  },
+  {
+    "revision": "033cdf4a086cad5b4a40",
+    "url": "/static/css/1.82927961.chunk.css"
+  },
+  {
+    "revision": "d8b0185ec2b3569af93dd928d9f9b7bd",
+    "url": "/index.html"
+  }
+];
